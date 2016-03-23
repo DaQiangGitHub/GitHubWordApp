7bc3e94d7c1466b11753159cd165f8cb1bf540e0
DaQiangGitHub <1140048346@qq.com>
2016-03-23 10:12:03 +0800

    第一次，未更改

---
+++ Gitaffdagad/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Gitaffdagad
+//
+//  Created by rimi on 16/3/23.
+//  Copyright © 2016年 zhugaoqiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
